033032405bc0f0b43a49e0ff7d4a8ad31cd3cfe1
Igor <Osmenodio@gmail.com>
2025-10-09 19:36:56 -0300

    Initial commit

---
+++ Igor.java
@@ -0,0 +1,58 @@
+package IGOR;
+
+import robocode.*;
+// import java.awt.Color;
+
+public class Igor extends Robot {
+
+    // Função principal do robô
+    public void run() {
+        // Cores opcionais
+        // setColors(Color.red, Color.blue, Color.green); // corpo, canhão, radar
+
+        while (true) {
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+
+    // Quando vê outro robô
+    public void onScannedRobot(ScannedRobotEvent e) {
+        // Mira no inimigo
+        double angleToEnemy = getHeading() + e.getBearing() - getGunHeading();
+        double turnGunAmount = normalRelativeAngleDegrees(angleToEnemy);
+        turnGunRight(turnGunAmount);
+
+        // Tiro com força baseada na distância
+        double distance = e.getDistance();
+        double firePower = 1;
+        if (distance < 200) {
+            firePower = 3;
+        } else if (distance < 500) {
+            firePower = 2;
+        }
+
+        fire(firePower);
+    }
+
+    // Quando é atingido por um tiro
+    public void onHitByBullet(HitByBulletEvent e) {
+        turnRight(90);
+        ahead(100);
+    }
+
+    // Quando bate na parede
+    public void onHitWall(HitWallEvent e) {
+        back(20);
+        turnRight(90);
+    }
+
+    // Método para normalizar o ângulo entre -180 e 180 graus
+    double normalRelativeAngleDegrees(double angle) {
+        while (angle > 180) angle -= 360;
+        while (angle < -180) angle += 360;
+        return angle;
+    }
+}
